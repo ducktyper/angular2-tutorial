4975afa7aad69170feb1ce0d78feea0ce703089a
Ducksan Cho <ducktyper@gmail.com>
2016-05-13 22:49:43 +1200

    Load component

--- src/app/angular2-tutorial.component.ts
+++ src/app/angular2-tutorial.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component'
 
 @Component({
   moduleId: module.id,
   selector: 'angular2-tutorial-app',
   templateUrl: 'angular2-tutorial.component.html',
-  styleUrls: ['angular2-tutorial.component.css']
+  styleUrls: ['angular2-tutorial.component.css'],
+  directives: [HeaderComponent]
 })
 export class Angular2TutorialAppComponent {
   title = 'angular2-tutorial works!';
