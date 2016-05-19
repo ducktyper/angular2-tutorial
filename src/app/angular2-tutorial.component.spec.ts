47be48268a9da6ec9338320e78819be525f237cb
Ducksan Cho <ducktyper@gmail.com>
2016-05-20 07:09:36 +1200

    Set property in ngOnInit using the service

--- src/app/angular2-tutorial.component.spec.ts
+++ src/app/angular2-tutorial.component.spec.ts
@@ -6,8 +6,9 @@ import {
   inject
 } from '@angular/core/testing';
 import { Angular2TutorialAppComponent } from '../app/angular2-tutorial.component';
+import { ProductService } from '../app/product.service';
 
-beforeEachProviders(() => [Angular2TutorialAppComponent]);
+beforeEachProviders(() => [Angular2TutorialAppComponent, ProductService]);
 
 describe('App: Angular2Tutorial', () => {
   it('should create the app',
@@ -18,5 +19,11 @@ describe('App: Angular2Tutorial', () => {
   it('should have as title \'angular2-tutorial works!\'',
       inject([Angular2TutorialAppComponent], (app: Angular2TutorialAppComponent) => {
     expect(app.title).toEqual('angular2-tutorial works!');
+  }));
+
+  it('should have products ',
+      inject([Angular2TutorialAppComponent], (app: Angular2TutorialAppComponent) => {
+    app.ngOnInit();
+    expect(app.products).toEqual([{name: "Apple"}, {name: "Orange"}]);
   }));
 });
--- src/app/angular2-tutorial.component.ts
+++ src/app/angular2-tutorial.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ProductService } from './product.service';
+import { Product } from './shared/product.model';
 
 @Component({
   moduleId: module.id,
   selector: 'angular2-tutorial-app',
   templateUrl: 'angular2-tutorial.component.html',
-  styleUrls: ['angular2-tutorial.component.css']
+  styleUrls: ['angular2-tutorial.component.css'],
+  providers: [ProductService],
 })
-export class Angular2TutorialAppComponent {
+export class Angular2TutorialAppComponent implements OnInit {
   title = 'angular2-tutorial works!';
+  products: Product[];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.products = this.productService.getProducts();
+  }
 }
